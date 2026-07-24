a07f563d6c1a9b68a4b4a2d154160b5dca5df38b
Celeste <u8140939@anu.edu.au>
2026-07-24 15:19:32 +1000

    change the name of integration test

--- src/test/java/com/example/movielocations/LocationRepositoryIntegrationTest.java
+++ src/test/java/com/example/movielocations/LocationRepositoryIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 @SpringBootTest
-class LocationRepositoryIT {
+class LocationRepositoryIntegrationTest {
 
     @Container
     @ServiceConnection
